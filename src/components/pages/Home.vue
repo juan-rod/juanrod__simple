109360892d5b9369d991a632e36f220ac150eb68
Juan Rodriguez <juan.j.rodriguez2@gmail.com>
2018-05-02 07:52:04 -0500

    first commit

---
+++ src/components/pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home border">
+      <div class="home__content maxW_768" 
+          data-tilt-scale="1.01" 
+          data-tilt-speed="100" 
+          data-tilt-perspective="500" 
+          @mouseover="mouseover($event)">
+          <h2>juan rodriguez</h2>
+      </div>
+  </div>
+</template>
+
+<script>
+import VanillaTilt from '../../utils/tilt.js'
+export default {
+  name: 'Home',
+  methods:{
+    mouseover (e) {
+      let className = e.target.classList[0]
+      const element = document.querySelector('.'+className);
+        VanillaTilt.init(element);
+    }
+   }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/router/routes.js
@@ -0,0 +1,10 @@
+import Home from '../components/pages/Home.vue'
+
+
+
+export const routes = [
+	{ 	
+		path: '/', 
+		component: Home
+	}
+]
